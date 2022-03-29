717ac6b7b1a37174be6320cc261972a86b1a65a8
Louis Lecouturier <71454059+LouisLecouturier@users.noreply.github.com>
2022-03-29 17:31:33 +0200

    Yss

---
+++ code/solver.py
@@ -0,0 +1,33 @@
+
+
+#    _______________
+#   /____/____/____/|
+#  /____/____/____/||
+# /____/____/____/|||
+# |    |    |    ||/|
+# |____|____|____|/||
+# |    |    |    ||/|
+# |____|____|____|/|/
+# |    |    |    ||/
+# |____|____|____|/
+
+
+# OOO OOO OOO YYY WWW GGG BBB YYY WWW GGG BBB YYY WWW GGG BBB RRR RRR RRR
+
+#     OOO
+#     OOO
+#     OOO
+# WWW GGG BBB YYY
+# YYY WWW GGG BBB
+# YYY WWW GGG BBB
+#     RRR
+#     RRR
+#     RRR
+
+
+# OOOOOOOOOWWWGGGBBBYYYYYYWWWGGGBBBYYYWWWGGGBBBRRRRRRRRR
+
+import optimal.solver as sv
+cubestring = 'DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL'
+
+sv.solve(cubestring, "")
